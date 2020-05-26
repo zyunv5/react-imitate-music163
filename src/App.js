621fa19ab3796c222c58dd736c39c310f571c524
zyunv5 <515224526@qq.com>
2020-05-27 00:29:17 +0800

    create project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store/index'
+import { GlobalStyle } from './style'
+import { renderRoutes } from 'react-router-config' //renderRoutes 读取路由配置转化为 Route 标签
+import { IconStyle } from './assets/iconfont/iconfont'
+import routes from './routes/index.js'
+import { HashRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/components/recommendList/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+    ListWrapper,
+    ListItem,
+    List
+  } from './style';
+
+  function RecommendList(props){
+      return(
+          <ListWrapper>
+              <h1 className="title">推荐歌单</h1>
+          </ListWrapper>
+      )
+  }
